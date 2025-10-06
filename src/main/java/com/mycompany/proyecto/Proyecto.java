5960e3603715faddabf53e24c4045fa77e2e7781
Victoria <h.victoria@correo.unimet.edu.ve>
2025-10-06 13:24:25 -0400

    prueba

--- src/main/java/com/mycompany/proyecto/Proyecto.java
+++ src/main/java/com/mycompany/proyecto/Proyecto.java
@@ -11,6 +11,7 @@ package com.mycompany.proyecto;
 public class Proyecto {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello !");
+        
     }
 }
