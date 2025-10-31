27269b4f7563021dc687d9743dbdd1bce53b4b69
Victoria <h.victoria@correo.unimet.edu.ve>
2025-10-31 14:57:00 -0400

    clase lista 2

---
+++ src/main/java/com/mycompany/proyecto/Lista.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto;
+
+/**
+ *
+ * @author victo
+ */
+public class Lista {
+   Nodo primero;
+   int size;
+   
+   public Lista (){
+       this.primero = null;
+       this.size = 0;
+   }
+}
