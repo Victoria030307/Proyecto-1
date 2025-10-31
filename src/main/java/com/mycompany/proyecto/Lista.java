690c3f384ad024fb5a2c934fdac42e23e56cc108
Victoria <h.victoria@correo.unimet.edu.ve>
2025-10-31 15:39:05 -0400

    clase lista cpmpleta

--- src/main/java/com/mycompany/proyecto/Lista.java
+++ src/main/java/com/mycompany/proyecto/Lista.java
@@ -9,11 +9,80 @@ package com.mycompany.proyecto;
  * @author victo
  */
 public class Lista {
-   Nodo primero;
-   int size;
-   
-   public Lista (){
-       this.primero = null;
-       this.size = 0;
-   }
+
+    Nodo primero;
+    int size;
+
+    public Lista() {
+        this.primero = null;
+        this.size = 0;
+    }
+
+    public void insertar(int dato) {
+        Nodo nuevo = new Nodo(dato);
+        if (this.primero == null) {
+            this.primero = nuevo;
+        
+
+        } else {
+            Nodo aux = this.primero;
+
+            while (aux.siguiente != null) {
+                aux = aux.siguiente;
+            }
+            aux.siguiente = nuevo;
+        }
+    }
+    
+    public void eliminar(int dato) {
+        Nodo aux = this.primero;
+        
+        if(this.primero == null){
+            return;
+        }
+        
+        if(aux.usuario == dato){
+            this.primero = aux.siguiente;
+        } else {
+            while(aux.siguiente != null && aux.siguiente.usuario != dato){
+                aux = aux.siguiente;
+            }
+            
+            if(aux.siguiente == null){
+                return;
+            }
+            aux.siguiente = aux.siguiente.siguiente;
+            
+
+        
+        }
+        
+        
+    }
+    
+    public String mostrar (){
+        String lista = "";
+        Nodo aux = this.primero;
+        
+        while(aux != null){
+            lista += aux.usuario + "\n";
+            aux = aux.siguiente;
+        }
+        
+        System.out.println(lista);
+  
+        return lista;
+    }
+    
+    public Nodo buscar (int dato){
+        Nodo aux = this.primero;
+
+        while(aux != null && aux.usuario != dato){
+            
+            aux = aux.siguiente;
+        }
+        return aux; 
+    }
+    
+    
 }
